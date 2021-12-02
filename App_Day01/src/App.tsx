67dd56bc2dcfb329ffb338d55057529802f8cedb
Marcos <you@example.com>
2021-12-02 03:16:19 -0600

    Finished App 01
    App 01 in day 01 is now finished

--- App_Day01/src/App.tsx
+++ App_Day01/src/App.tsx
@@ -1,10 +1,25 @@
 import "./styles.css";
+import { useState } from "react";
+
+
+const initialGifts = [
+  { name: "Mouse" },
+  { name: "Smarthphone" },
+  { name: "A hug" },
+  {name: "A Tatoo"}
+];
 
 export default function App() {
+  const [gifts, setGifts] = useState(initialGifts);
+
   return (
     <div className="App">
       <h1>Regalos:</h1>
-      ...
+      <ul >
+        {gifts.map((gift, id) => (
+          <li key={id}>{gift.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
