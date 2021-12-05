c3abf3245cd10684f43e03b0cfcda066dd980fb7
Marcos <43713105+markconfig@users.noreply.github.com>
2021-12-05 00:42:16 -0600

    Finished App 02

--- App_Day02/src/App.tsx
+++ App_Day02/src/App.tsx
@@ -10,8 +10,7 @@ export default function App() {
       <ul>
         {gifts.map((gift, id) => (
           <li key={id}>{gift.name}</li>
-        ))
-        }
+        ))}
       </ul>
     </div>
   );
