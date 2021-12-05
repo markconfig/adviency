74761c4b846a6de55951d1a85bb9a8d420fd6f90
Marcos <you@example.com>
2021-12-05 00:36:36 -0600

    Initial Comit

--- App_Day02/src/App.tsx
+++ App_Day02/src/App.tsx
@@ -1,10 +1,17 @@
 import "./styles.css";
+import React, { useState } from "react";
 
+const defaultGifts = [{ name: "A computer" }, { name: "A iPhone" }, { name: "Food" }, { name: "A SkateBoard" }]
 export default function App() {
+  const [gifts, setGifts] = useState(defaultGifts)
   return (
-    <div className="App">
+    <div className="container">
       <h1>Regalos:</h1>
-      ...
+      <ul>
+        {gifts.map((gift, id) => (
+          <li key={id}>{gift.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
