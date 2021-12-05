1e23294c13d38482d6aa2858d528d1ddd38b3ec2
Marcos <you@example.com>
2021-12-05 02:58:26 -0600

    Finished App 04

---
+++ App_Day04/src/App.tsx
@@ -0,0 +1,57 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+interface Gift {
+  name: string
+}
+const defaultGifts: Gift[] = []
+const defaultNewGift: Gift = { name: "" }
+export default function App() {
+  const [gifts, setGifts] = useState(defaultGifts)
+  const [newGift, setNewGift] = useState(defaultNewGift)
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setNewGift({ name: e.target.value })
+  }
+  function handleSaveGift() {
+    setGifts([...gifts, newGift])
+    setNewGift({ name: "" })
+  }
+  function handleDeleteGift(id: number) {
+    const tempGift = [...gifts]
+    tempGift.splice(id, 1)
+    setGifts(tempGift)
+  }
+  return (
+    <div className="container">
+      <h1>Regalos:</h1>
+      <form onSubmit={
+        (e) => {
+          e.preventDefault();
+          handleSaveGift();
+        }
+      }>
+        <input
+          placeholder="¡Agrega Un Realo!"
+          required
+          key="name"
+          name="name"
+          onChange={handleInputChange}
+          value={newGift.name}
+          ref={(input) => input && input.focus()}
+        ></input>
+        <button type="submit" className="button-add">+ Agregar</button>
+      </form>
+
+      <ul>
+        {gifts.map((gift, id) => (
+          <li key={id}>🎀{gift.name}
+            <button key={id} className="button-delete" onClick={() => handleDeleteGift(id)}
+            >Eliminar
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
